60d0f2f523b646ec2567e940019f09605447b709
Jasmine Huang <tinjasminee@gmail.com>
2020-10-26 21:07:28 -0700

    first_session_note

---
+++ lesson1.r
@@ -0,0 +1,30 @@
+#intro
+2+1
+1/200
+2**3
+
+#assign variables
+#Variable names are case sensitive
+x <- 32
+x
+X <-20
+X
+
+#creating new variable
+#need to execute each line to assign variable
+a <- 5
+b <- 6
+a + b
+c <- a+b
+c
+
+#Numeric: decimals values like 1.3
+#Integer
+#logical: True or False
+#Characters: "Lucy"
+
+my_logical <- FALSE
+
+#can reassign value(same or different data type) to existing variable
+my_logical <- 52
+my_logical
