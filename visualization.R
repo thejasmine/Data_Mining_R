0084a75ad066d1ca06c710aa19d392e927b14cd2
Jasmine Huang <tinjasminee@gmail.com>
2020-11-02 21:13:29 -0800

    visual

--- visualization.R
+++ visualization.R
@@ -1,5 +1,29 @@
 #ggplot
+#create a coordinate system
 install.packages("ggplot2")
-
 library(ggplot2)
-ggplot( data = mpg)
+
+#create scatter plot
+View(mpg)
+
+#column name
+names(mpg)
+
+#create scatter plot
+ggplot( data = mpg) + 
+  geom_point(aes(x=displ, y=hwy, color=class)) + 
+  labs(y="highway miles per gallon")
+
+#grammer of graphics
+#specify plot building blocks and combine them
+
+mower.df = read.csv("RidingMowers.csv", stringsAsFactors = TRUE)
+names(df)
+ggplot(data=mower.df) + geom_point( aes(x = Income, y=Lot_Size, color=Ownership))
+
+
+#bar chart
+ggplot(data=mpg) + geom_bar(aes(x=class))
+
+
+
